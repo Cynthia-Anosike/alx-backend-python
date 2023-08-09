1a08b9b2f976b3c76e1a0a26d460e0f2eeb9f866
Cynthia Anosike <chiomaanosike28@gmail.com>
2023-08-09 11:56:57 +0100

    float add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-''' A function annotation to add and return a float'''
+''' A function annotation to add and return a float
+'''
 
 def add(a: float, b: float) -> float:
-    ''' an add function that takes two arguments and return a float'''
+    ''' an add function that takes two arguments and return a float
+    '''
     return a + b
 
