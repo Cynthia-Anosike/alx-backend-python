7efd454e042c81a9500fb51a82daa84fc06a65e8
Cynthia Anosike <chiomaanosike28@gmail.com>
2023-08-09 12:11:18 +0100

    task1

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,8 +2,8 @@
 ''' A function annotation to add and return a float
 '''
 
+
 def add(a: float, b: float) -> float:
     ''' an add function that takes two arguments and return a float
     '''
     return a + b
-
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' A type-annotated function to add two strings
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' This function takes two strings and concatenate them
+    '''
+    return str1 + str2
