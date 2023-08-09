737d07b6262b3d7e8faefc9da0f33fcbf17af5cf
Cynthia Anosike <chiomaanosike28@gmail.com>
2023-08-09 14:04:22 +0100

    sum list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+''' A code that computes complex types
+'''
+
+
+input_list: list[float] = []
+
+
+def sum_list(input_list) -> float:
+    ''' This function takes the list args and return their sum
+    '''
+    return float(sum(input_list))
